f911b02ca8255ff9b1ce2237cac5e48ef3880c03
Gana <ganapathi12131@gmail.com>
2026-01-01 20:45:08 +0530

    catalogue management with empty business login

---
+++ src/main/java/com/backend/mastery/catalogue/management/CatalogueManagementApplication.java
@@ -0,0 +1,13 @@
+package com.backend.mastery.catalogue.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogueManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/mastery/catalogue/management/CatalogueManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.mastery.catalogue.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
